05c7bf40923b40878e719d823cf728beab97b25d
Yogesh Kotadiya <yogeshkotadiya007@gmail.com>
2018-04-07 21:28:55 +0530

    Added Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,4 +4,16 @@ module.exports = {
   },
   plugins: ['gatsby-plugin-react-helmet'],
   pathPrefix: `/yogeshkotadiya.github.io`,
+  {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: "UA-117149846-1",
+      // Puts tracking script in the head instead of the body
+      head: false,
+      // Setting this parameter is optional
+      anonymize: true,
+      // Setting this parameter is also optional
+      respectDNT: true,
+    },
+  },
 };

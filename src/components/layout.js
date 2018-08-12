9caf5ef626aef5467cf89e18cf7e74e111c46ec7
Yogesh Kotadiya <yogeshkotadiya007@gmail.com>
2018-08-13 02:24:30 +0530

    Migration to V2 Pre-Migration Changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import Header from '../components/Header'
-import './index.css'
+import '../layouts/index.css'
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -36,7 +36,7 @@ const TemplateWrapper = ({ children }) => (
         paddingTop: 0,
       }}
     >
-      {children()}
+      {children}
     </div>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import {Link} from 'gatsby';
+import Layout form  '../components/layout';
 
 const IndexPage = () => (
   <div>
